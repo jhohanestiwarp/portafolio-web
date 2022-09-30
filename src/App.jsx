b9923477b18dbaf10f27138ea04057a5200658d8
proyecto_utch <jhohanestiwarpalaciosmena99@gmail.com>
2022-09-30 17:09:11 -0500

    cambio crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+  
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
@@ -10,7 +10,9 @@ import Tecnologias from './components/Tecnologias'
 import Todo from '../src/assets/todo.png'
 import AppClima from '../src/assets/aap-climas.png'
 import CrudAngular from '../src/assets/crud-angular.png'
+import app from '../src/assets/evidencia.png'
 import { Title } from './components/Title'
+
 
 
 function App() {
@@ -29,23 +31,29 @@ function App() {
           descripcion={
             "Todo realizado con React, usando formularios, manejo de hooks y LocalStorage"
           }
-          ver=""
+          ver="https://app-de-tareas-8d875.web.app/"
         />
+      
+        <Proyectos
+          imagen={app}
+          descripcion={"Crud con angular material"}
+          ver="https://app-crud-angular.netlify.app/"
+        />
+        
         <Proyectos
           imagen={AppClima}
           title={"Aplicación del clima"}
           descripcion={
-            "Aplicacion del clima usando Angular, usando HTTP client para llamados a API y formularios"
-          }
-          ver=""
+            "Aplicacion del clima usando Angular, usando HTTP client para llamados a API y formularios"}
+          ver="https://app-clima-c5f22.web.app/" 
         />
+        
         <Proyectos
           imagen={CrudAngular}
-          title={"Crud"}
-          descripcion={"Crud con Angular, usando Angular Material"}
-          ver=""
+          title={"Aplicativo web de contrataciones"}
+          descripcion={"Aplicativo web de contrataciones para la alcaldia municipal de Qubibdo creado en java SPRINTBOOT previamente consumido en angular, Con Roles usando los Guards de angular"}
+          ver="https://proyecto-app-a9bca.web.app/"
         />
-      
           
       
         <Footer />
